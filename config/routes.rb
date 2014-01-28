dc62ed86fb7186b66c8521f20a6db045e91812c8
Michael Gerber <mgerber2@comcast.net>
2014-01-28 04:53:53 -0400

    Some incredible stuff here! Woohoo!

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 FirstApp::Application.routes.draw do
+  get "ccsf_rails/index"
+  get "ccsf_rails/links"
+  get "ccsf_rails/about"
   get "welcome/index"
   root 'welcome#index'
 # The priority is based upon order of creation: first created -> highest priority.
